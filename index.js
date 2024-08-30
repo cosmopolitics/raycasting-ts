c833da0b88caf2778b3e9a652a3d6209cb9062c4
cosmo <atticuswaterbury@gmail.com>
2024-08-29 19:11:42 -0700

    added 'third' dimension

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 const eps = 1e-6;
+const nearClippingPlane = 1.0;
+const farClippingPlane = 10.0;
+const fov = Math.PI * 0.5;
+const resol = 400;
+const player_speed = 0.25;
 class vec2 {
     constructor(x, y) {
         this.x = x;
@@ -7,6 +12,9 @@ class vec2 {
     }
     static zero() {
         return new vec2(0, 0);
+    }
+    static fromangle(angle) {
+        return new vec2(Math.cos(angle), Math.sin(angle));
     }
     add(that) {
         return new vec2(this.x + that.x, this.y + that.y);
@@ -25,15 +33,27 @@ class vec2 {
     }
     norm() {
         const one = this.length();
-        if (one == 0)
+        if (one === 0)
             return new vec2(0, 0);
         return new vec2(this.x / one, this.y / one);
     }
     scale(value) {
         return new vec2(this.x * value, this.y * value);
     }
-    distanceto(that) {
-        return that.sub(this).length();
+    rot90() {
+        return new vec2(-this.y, this.x);
+    }
+    lerp(that, t) {
+        return that.sub(this).scale(t).add(this);
+    }
+    dot(that) {
+        return this.x * that.x + this.y * that.y;
+    }
+    sqrlen() {
+        return (this.x * this.x + this.y * this.y);
+    }
+    squaredist(that) {
+        return that.sub(this).sqrlen();
     }
     array() {
         return [this.x, this.y];
@@ -77,7 +97,7 @@ function raystep(p1, p2) {
             const y3 = snap(p2.y, d.y);
             const x3 = (y3 - c) / k;
             let p3t = new vec2(x3, y3);
-            if (p2.distanceto(p3t) < p2.distanceto(p3)) {
+            if (p2.squaredist(p3t) < p2.squaredist(p3)) {
                 p3 = p3t;
             }
         }
@@ -88,6 +108,18 @@ function raystep(p1, p2) {
         p3 = new vec2(x3, y3);
     }
     return p3;
+}
+function castray(scene, p1, p2) {
+    let start = p1;
+    while (start.squaredist(p1) < farClippingPlane ** 2) {
+        const c = hitcell(p1, p2);
+        if (insidescene(scene, c) && scene[c.y][c.x] !== null)
+            break;
+        const p3 = raystep(p1, p2);
+        p1 = p2;
+        p2 = p3;
+    }
+    return p2;
 }
 function canvassize(ctx) {
     return new vec2(ctx.canvas.width, ctx.canvas.height);
@@ -100,18 +132,36 @@ function scenesize(scene) {
     }
     return new vec2(x, y);
 }
-function minimap(ctx, p1, p2, position, size, scene) {
-    ctx.reset();
-    ctx.fillStyle = "#181818";
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+function insidescene(scene, p) {
+    const size = scenesize(scene);
+    return 0 <= p.x && p.x < size.x && 0 <= p.y && p.y < size.y;
+}
+class Player {
+    constructor(position, direction) {
+        this.position = position;
+        this.direction = direction;
+    }
+    fov() {
+        const l = Math.tan(fov * 0.5) * nearClippingPlane;
+        let p = this.position.add(vec2.fromangle(this.direction).scale(nearClippingPlane));
+        const p1 = p.sub(p.sub(this.position).rot90().norm().scale(l));
+        const p2 = p.add(p.sub(this.position).rot90().norm().scale(l));
+        return [p1, p2];
+    }
+}
+function renderminimap(ctx, player, position, size, scene) {
+    ctx.save();
     const gridsize = scenesize(scene);
-    ctx.scale(...size.div(gridsize).array());
     ctx.translate(...position.array());
-    ctx.lineWidth = 0.01;
+    ctx.scale(...size.div(gridsize).array());
+    ctx.fillStyle = "#181818";
+    ctx.fillRect(0, 0, ...gridsize.array());
+    ctx.lineWidth = 0.06;
     for (let y = 0; y < gridsize.y; y++) {
         for (let x = 0; x < gridsize.x; x++) {
-            if (scene[y][x] !== 0) {
-                ctx.fillStyle = "#303030";
+            const color = scene[y][x];
+            if (color !== null) {
+                ctx.fillStyle = color;
                 ctx.fillRect(x, y, 1, 1);
             }
         }
@@ -124,50 +174,93 @@ function minimap(ctx, p1, p2, position, size, scene) {
         strokeline(ctx, new vec2(0, i), new vec2(gridsize.x, i));
     }
     ctx.fillStyle = "purple";
-    fillcircle(ctx, p1, 0.2);
-    if (p2 !== undefined) {
-        for (;;) {
-            fillcircle(ctx, p2, 0.2);
-            ctx.strokeStyle = "magenta";
-            strokeline(ctx, p1, p2);
-            const c = hitcell(p1, p2);
-            if (c.x < 0
-                || c.x >= gridsize.x || c.y < 0
-                || c.y >= gridsize.y || scene[c.y][c.x] == 1)
-                break;
-            const p3 = raystep(p1, p2);
-            p1 = p2;
-            p2 = p3;
+    fillcircle(ctx, player.position, 0.2);
+    const [p1, p2] = player.fov();
+    ctx.strokeStyle = "purple";
+    strokeline(ctx, p1, p2);
+    strokeline(ctx, player.position, p1);
+    strokeline(ctx, player.position, p2);
+    ctx.restore();
+}
+function renderscene(ctx, player, scene) {
+    const stripwidth = Math.ceil(ctx.canvas.width / resol);
+    const [r1, r2] = player.fov();
+    for (let x = 0; x < resol; ++x) {
+        const p = castray(scene, player.position, r1.lerp(r2, x / resol));
+        const c = hitcell(player.position, p);
+        if (insidescene(scene, c)) {
+            const color = scene[c.y][c.x];
+            if (color !== null) {
+                const v = p.sub(player.position);
+                const d = vec2.fromangle(player.direction);
+                const stripheight = ctx.canvas.height / v.dot(d);
+                ctx.fillStyle = color;
+                ctx.fillRect(x * stripwidth, (ctx.canvas.height - stripheight) * 0.5, stripwidth, stripheight);
+            }
         }
     }
 }
+function rendergame(ctx, player, scene) {
+    const minimappos = vec2.zero().add(canvassize(ctx).scale(0.02));
+    const cellsize = ctx.canvas.width * 0.02;
+    const minimapsize = scenesize(scene).scale(cellsize);
+    ctx.fillStyle = "#181818";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    renderscene(ctx, player, scene);
+    renderminimap(ctx, player, minimappos, minimapsize, scene);
+}
 (() => {
-    let scene = [
-        [0, 0, 1, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ];
     const game = document.getElementById("game");
-    if (game === null) {
+    if (game === null)
         throw new Error("no game found ");
-    }
     const ctx = game.getContext("2d");
-    if (ctx === null) {
+    if (ctx === null)
         throw new Error("2d ctx is not supported");
-    }
-    // p2 follows the mouse
-    let p1 = scenesize(scene).mul(new vec2(0.43, 0.33));
-    let p2 = undefined;
-    game.addEventListener("mousemove", (mouseevent) => {
-        p2 = new vec2(mouseevent.offsetX, mouseevent.offsetY)
-            .div(canvassize(ctx))
-            .mul(scenesize(scene));
-        minimap(ctx, p1, p2, vec2.zero(), canvassize(ctx), scene);
+    const factor = 100;
+    game.width = 16 * factor;
+    game.height = 9 * factor;
+    let scene = [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, "cyan", "purple", null, null, null, null, null],
+        [null, null, null, null, "white", null, null, null, null, null],
+        [null, null, "red", "green", "blue", null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+    ];
+    const player = new Player(scenesize(scene).mul(new vec2(0.63, 0.63)), Math.PI * 1.25);
+    window.addEventListener("keydown", (e) => {
+        switch (e.code) {
+            case 'KeyW':
+                {
+                    player.position = player.position
+                        .add(vec2.fromangle(player.direction).scale(player_speed));
+                    rendergame(ctx, player, scene);
+                }
+                break;
+            case 'KeyS':
+                {
+                    player.position = player.position
+                        .sub(vec2.fromangle(player.direction).scale(player_speed));
+                    rendergame(ctx, player, scene);
+                }
+                break;
+            case 'KeyD':
+                {
+                    player.direction += Math.PI * 0.025;
+                    rendergame(ctx, player, scene);
+                }
+                break;
+            case 'KeyA':
+                {
+                    player.direction -= Math.PI * 0.025;
+                    rendergame(ctx, player, scene);
+                }
+                break;
+        }
     });
-    minimap(ctx, p1, p2, vec2.zero(), canvassize(ctx), scene);
-    console.log("hellop");
+    rendergame(ctx, player, scene);
 })();
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 const eps = 1e-6;
+const nearClippingPlane = 1.0;
+const farClippingPlane = 10.0
+const fov = Math.PI*0.5
+const resol = 400;
+const player_speed = 0.25;
 
 class vec2 {
   x: number;
@@ -10,6 +15,9 @@ class vec2 {
   }
   static zero(): vec2 {
     return new vec2(0, 0);
+  }
+  static fromangle(angle: number): vec2 {
+    return new vec2(Math.cos(angle), Math.sin(angle));
   }
   add(that: vec2): vec2 {
     return new vec2(this.x+that.x, this.y+that.y);
@@ -28,14 +36,26 @@ class vec2 {
   }
   norm(): vec2 {
     const one = this.length();
-    if (one == 0) return new vec2(0,0);
+    if (one === 0) return new vec2(0,0);
     return new vec2(this.x/one, this.y/one);
   }
   scale(value: number): vec2 {
     return new vec2(this.x*value, this.y*value)
   }
-  distanceto(that:vec2) {
-    return that.sub(this).length();
+  rot90(): vec2 {
+    return new vec2(-this.y, this.x);
+  }
+  lerp(that: vec2, t: number): vec2 {
+    return that.sub(this).scale(t).add(this);
+  }
+  dot(that: vec2):number {
+    return this.x*that.x + this.y*that.y;
+  }
+  sqrlen(): number {
+    return (this.x*this.x + this.y*this.y)
+  }
+  squaredist(that: vec2) {
+    return that.sub(this).sqrlen();
   }
   array(): [number, number] {
     return [this.x, this.y];
@@ -50,10 +70,10 @@ function fillcircle(ctx: CanvasRenderingContext2D, center: vec2, radius: number)
 }
 
 function strokeline(ctx: CanvasRenderingContext2D, p1: vec2, p2: vec2) {
-    ctx.beginPath();
-    ctx.moveTo(...p1.array());
-    ctx.lineTo(...p2.array());
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(...p1.array());
+  ctx.lineTo(...p2.array());
+  ctx.stroke();
 }
 
 function snap(x: number, dx:number): number {
@@ -85,24 +105,36 @@ function raystep(p1: vec2, p2: vec2): vec2 {
       const y3 = snap(p2.y, d.y);
       const x3 = (y3 - c)/k;
       let p3t = new vec2(x3,y3)
-      if (p2.distanceto(p3t) < p2.distanceto(p3)) {
+      if (p2.squaredist(p3t) < p2.squaredist(p3)) {
         p3 = p3t;
       }
     }
   } else {
-      const y3 = snap(p2.y, d.y);
-      const x3 = p2.x
-      p3 = new vec2(x3, y3);
+    const y3 = snap(p2.y, d.y);
+    const x3 = p2.x
+    p3 = new vec2(x3, y3);
   }
 
   return p3;
+}
+
+function castray(scene: Scene, p1: vec2, p2: vec2): vec2 {
+  let start = p1;
+  while (start.squaredist(p1) < farClippingPlane**2) {
+    const c = hitcell(p1,p2);
+    if(insidescene(scene, c) && scene[c.y][c.x] !== null) break;
+    const p3 = raystep(p1,p2);
+    p1 = p2;
+    p2 = p3;
+  }
+  return p2;
 }
 
 function canvassize(ctx: CanvasRenderingContext2D): vec2 {
   return new vec2(ctx.canvas.width, ctx.canvas.height);
 }
 
-type Scene = Array<Array<number>>;
+type Scene = Array<Array<string | null>>;
 
 function scenesize(scene: Scene): vec2 {
   const y = scene.length;
@@ -113,22 +145,45 @@ function scenesize(scene: Scene): vec2 {
   return new vec2(x,y);
 }
 
-function minimap(ctx: CanvasRenderingContext2D, p1: vec2, p2: vec2 | undefined, position: vec2, size: vec2, scene: Array<Array<number>>) {
-  ctx.reset();
+function insidescene(scene: Scene, p: vec2):boolean {
+  const size = scenesize(scene)
+  return 0 <= p.x && p.x < size.x && 0 <= p.y && p.y < size.y ;
+}
 
-  ctx.fillStyle = "#181818"
-  ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+class Player {
+  position: vec2;
+  direction: number;
+  constructor(position: vec2, direction: number) {
+    this.position = position;
+    this.direction = direction;
+  }
+  fov(): [vec2, vec2] {
+    const l = Math.tan(fov*0.5)*nearClippingPlane;
+    let p = this.position.add(vec2.fromangle(this.direction).scale(nearClippingPlane));
+    const p1 = p.sub(p.sub(this.position).rot90().norm().scale(l));
+    const p2 = p.add(p.sub(this.position).rot90().norm().scale(l));
+    return [p1, p2]
+  }
+}
+
+function renderminimap(ctx: CanvasRenderingContext2D, player: Player , position: vec2, size: vec2, scene: Scene) {
+  ctx.save();
 
   const gridsize = scenesize(scene)
 
-  ctx.scale(...size.div(gridsize).array());
   ctx.translate(...position.array());
-  ctx.lineWidth = 0.01;
+  ctx.scale(...size.div(gridsize).array());
+
+  ctx.fillStyle = "#181818";
+  ctx.fillRect(0,0,...gridsize.array());
+
+  ctx.lineWidth = 0.06;
 
   for (let y = 0; y < gridsize.y; y++) {
     for(let x = 0; x < gridsize.x; x++) {
-      if (scene[y][x] !== 0) {
-        ctx.fillStyle = "#303030"
+      const color = scene[y][x];
+      if (color !== null) {
+        ctx.fillStyle = color;
         ctx.fillRect(x,y,1,1);
       }
     }
@@ -143,56 +198,105 @@ function minimap(ctx: CanvasRenderingContext2D, p1: vec2, p2: vec2 | undefined, 
   }
 
   ctx.fillStyle = "purple"
-  fillcircle(ctx, p1, 0.2)
-  if (p2 !== undefined) {
-    for (;;) {
-      fillcircle(ctx, p2, 0.2)
-      ctx.strokeStyle = "magenta"
-      strokeline(ctx, p1, p2)
+  fillcircle(ctx, player.position, 0.2)
 
-      const c: vec2 = hitcell(p1,p2);
-      if (c.x < 0 
-        || c.x >= gridsize.x || c.y < 0 
-        || c.y >= gridsize.y || scene[c.y][c.x] == 1) break;
+  const [p1,p2] = player.fov();
 
-      const p3 = raystep(p1, p2)
-      p1 = p2;
-      p2 = p3;
+  ctx.strokeStyle = "purple"
+  strokeline(ctx, p1, p2)
+  strokeline(ctx, player.position, p1)
+  strokeline(ctx, player.position, p2)
+
+  ctx.restore();
+}
+
+function renderscene(ctx: CanvasRenderingContext2D, player: Player, scene: Scene) {
+  const stripwidth = Math.ceil(ctx.canvas.width/resol);
+  const [r1,r2] = player.fov();
+
+  for (let x = 0; x < resol; ++x) {
+    const p = castray(scene, player.position, r1.lerp(r2, x/resol))
+    const c = hitcell(player.position, p)
+
+    if (insidescene(scene, c)) {
+      const color = scene[c.y][c.x];
+      if (color !== null) {
+        const v = p.sub(player.position);
+        const d = vec2.fromangle(player.direction);
+
+        const stripheight = ctx.canvas.height/v.dot(d);
+        ctx.fillStyle = color;
+        ctx.fillRect(x*stripwidth, (ctx.canvas.height - stripheight)*0.5
+                     , stripwidth, stripheight);
+      }
     }
   }
 }
 
+
+function rendergame(ctx: CanvasRenderingContext2D, player: Player, scene: Scene) {
+  const minimappos = vec2.zero().add(canvassize(ctx).scale(0.02));
+  const cellsize = ctx.canvas.width*0.02
+  const minimapsize = scenesize(scene).scale(cellsize);
+
+  ctx.fillStyle = "#181818";
+  ctx.fillRect(0,0, ctx.canvas.width, ctx.canvas.height);
+
+  renderscene(ctx, player, scene);
+  renderminimap(ctx, player, minimappos, minimapsize, scene);
+}
+
 (() => {
+  const game = document.getElementById("game") as (HTMLCanvasElement | null);
+  if (game === null ) throw new Error("no game found ");
+  const ctx = game.getContext("2d");
+  if (ctx === null ) throw new Error("2d ctx is not supported");
+
+  const factor = 100;
+  game.width = 16*factor;
+  game.height = 9*factor;
+
   let scene = [
-    [0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, "cyan", "purple", null, null, null, null, null],
+    [null, null, null, null, "white", null, null, null, null, null],
+    [null, null, "red", "green", "blue", null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
   ]
 
-  const game = document.getElementById("game") as (HTMLCanvasElement | null);
-  if (game === null ) {
-    throw new Error("no game found ");
-  }
+  const player = new Player(scenesize(scene).mul( new vec2(0.63, 0.63)),
+                            Math.PI*1.25);
 
-  const ctx = game.getContext("2d");
-  if (ctx === null ) {
-    throw new Error("2d ctx is not supported");
-  }
+  window.addEventListener("keydown", (e) => {
+    switch (e.code) {
+      case 'KeyW': {
+        player.position = player.position
+          .add(vec2.fromangle(player.direction).scale(player_speed))
 
-  // p2 follows the mouse
-  let p1 = scenesize(scene).mul( new vec2(0.43, 0.33));
-  let p2: vec2 | undefined = undefined
-  game.addEventListener("mousemove", (mouseevent) => {
-    p2 = new vec2(mouseevent.offsetX, mouseevent.offsetY)
-      .div(canvassize(ctx))
-      .mul(scenesize(scene));
-    minimap(ctx, p1, p2, vec2.zero(), canvassize(ctx), scene);
+        rendergame(ctx, player, scene);
+      } break;
+      case 'KeyS': {
+        player.position = player.position
+          .sub(vec2.fromangle(player.direction).scale(player_speed))
+
+        rendergame(ctx, player, scene);
+      } break;
+      case 'KeyD': {
+        player.direction += Math.PI*0.025;
+
+        rendergame(ctx, player, scene);
+      } break;
+      case 'KeyA': {
+        player.direction -= Math.PI*0.025;
+
+        rendergame(ctx, player, scene);
+      } break;
+    }
   });
-  minimap(ctx, p1, p2, vec2.zero(), canvassize(ctx), scene);
 
-  console.log("hellop");
+  rendergame(ctx, player, scene);
 })()
